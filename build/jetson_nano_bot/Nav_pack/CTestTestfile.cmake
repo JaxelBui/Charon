bfae40e0ebb21248c410594d45d14a930ea1bf29
Doanratruong <trongtuong2010@gmail.com>
2024-08-05 21:27:37 +0700

    IMU

--- build/jetson_nano_bot/Nav_pack/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rasp/catkin_ws/src/jetson_nano_bot/Nav_pack
-# Build directory: /home/rasp/catkin_ws/build/jetson_nano_bot/Nav_pack
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/jetson_nano_bot/launch_jetson_nano_bot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rasp/catkin_ws/src/jetson_nano_bot/launch_jetson_nano_bot
-# Build directory: /home/rasp/catkin_ws/build/jetson_nano_bot/launch_jetson_nano_bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
